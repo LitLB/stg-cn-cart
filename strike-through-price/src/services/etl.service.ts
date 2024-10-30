d724de13e59ce51cdc5355f7972e4420993b52c8
Phawittra Pipitsuntorn <phawittra.pip@ascendcorp.com>
2024-10-30 19:21:00 +0700

    add logs

--- strike-through-price/src/services/etl.service.ts
+++ strike-through-price/src/services/etl.service.ts
@@ -3,15 +3,17 @@ import { ETLConfig } from '../utils/config.utils';
 import { logger } from '../utils/logger.utils'
 
 export const callPDPWorkflow = async (campaigns: any) => {
+    const payload = {
+        workflowName: 'CMSPDPCampaign',
+        workflowId: `ETL-CMS-PDP-Campaign-${ Date.now() }`,
+        taskQueue: 'cms-pdp-campaign-task-queue',
+        input: campaigns
+    }
+    logger.info(`Call cms-pdp-campaign-task-queue payload: ${JSON.stringify(payload)}`)
     axios({
         method: 'post',
         url: ETLConfig().clientUrl,
-        data: {
-            workflowName: 'CMSPDPCampaign',
-            workflowId: `ETL-CMS-PDP-Campaign-${ Date.now() }`,
-            taskQueue: 'cms-pdp-campaign-task-queue',
-            input: campaigns
-        }
+        data: payload
     })
     .then((res: any) => {
         logger.info(`Call cms-pdp-campaign-task-queue status: ${res.status} data: ${JSON.stringify(res.data)}`)
