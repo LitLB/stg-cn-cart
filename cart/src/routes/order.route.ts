f72b88fa67e131e1bfd7baa136dc85e84b742c44
Yunyong Utama <yunyong.uta@ascendcorp.com>
2025-02-17 16:47:42 +0700

    feat: remove auth

--- cart/src/routes/order.route.ts
+++ cart/src/routes/order.route.ts
@@ -2,7 +2,6 @@
 
 import { Router } from 'express';
 import { OrderController } from '../controllers/order.controller';
-import { authenticate } from '../middleware/authenticate';
 import { validateRequest } from '../middleware/validate.middleware';
 import { getOrderTrackingQuerySchema, getOrderTrackingParamsSchema } from '../schemas/order.schema';
 
@@ -12,6 +11,6 @@ const orderController = new OrderController();
 orderRouter.get('/v1/orders/:id', orderController.getOrderById);
 orderRouter.post('/v1/orders/:orderId/tnc', orderController.setTncOrder);
 
-orderRouter.get('/v1/tracking/:orderNumber', authenticate, validateRequest({ params: getOrderTrackingParamsSchema, query: getOrderTrackingQuerySchema }), orderController.getOrderTracking);
+orderRouter.get('/v1/tracking/:orderNumber', validateRequest({ params: getOrderTrackingParamsSchema, query: getOrderTrackingQuerySchema }), orderController.getOrderTracking);
 
 export default orderRouter;
