569e769370af888ddc9d081b19cb4ea2845cf642
devvictrix <dev.victrix@gmail.com>
2025-05-14 15:35:23 +0700

    feat: customer verification (3)

--- verify/src/controllers/otp.controller.ts
+++ verify/src/controllers/otp.controller.ts
@@ -15,7 +15,6 @@ export class OtpController {
         this.otpService = new OtpService();
         this.requestOtp = this.requestOtp.bind(this);
         this.verifyOtp = this.verifyOtp.bind(this);
-        this.getCustomerProfile = this.getCustomerProfile.bind(this);
         this.getPackageOffer = this.getPackageOffer.bind(this);
         this.handleCustomerVerification = this.handleCustomerVerification.bind(this);
     }
@@ -65,41 +64,6 @@ export class OtpController {
             });
 
         } catch (err: any) {
-            next(err);
-        }
-    }
-
-    public async getCustomerProfile(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> {
-        const logModel = createLogModel(LOG_APPS.STORE_WEB, "");
-        logModel.start_date = moment().toISOString();
-        LogModel.initialize(logModel);
-
-        try {
-            const {
-                journey,
-                verifyState,
-                mobileNumber
-            } = req.query as unknown as checkCustomerProfileRequest;
-
-            const { correlatorid } = req.headers
-
-            const responseBody = await this.otpService.getCustomerProfile(
-                correlatorid as string,
-                journey,
-                verifyState,
-                mobileNumber,
-            );
-
-            res.status(200).json({
-                statusCode: HTTP_STATUSES.OK,
-                statusMessage: HTTP_MESSAGE.OK,
-                data: responseBody
-            });
-        } catch (err) {
             next(err);
         }
     }
