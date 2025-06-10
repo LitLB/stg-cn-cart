90aab8e1b58a0fb41b4afd712ad0c295ddec69de
chutiphong.cha <chutiphong.charoenchao@gmail.com>
2025-06-10 11:47:23 +0700

    feat: add validate select body

--- cart/src/schemas/cart-item.schema.ts
+++ cart/src/schemas/cart-item.schema.ts
@@ -67,12 +67,25 @@ export function validateSelectCartItemBody(body: any) {
 							'string.empty': 'Package Code cannot be empty',
 							'any.required': 'Package Code is required',
 						}),
+						advancePayment: Joi.number().optional().allow(0),
+						contractTerm: Joi.number().optional().allow(0),
+						penalty: Joi.number().optional().allow(0),
 					}).optional(),
 					sim: Joi.object({
 						sku: Joi.string().required().messages({
 							'string.empty': 'SIM SKU cannot be empty',
 							'any.required': 'SIM SKU is required',
 						}),
+					}).optional(),
+					bundleProduct: Joi.object({
+						key: Joi.string().required().messages({
+							'string.empty': 'Bundle Product Key cannot be empty',
+							'any.required': 'Bundle Product Key is required',
+						}),
+						promotionSetCode: Joi.string().required().messages({
+							'string.empty': 'Promotion Set Code cannot be empty',
+							'any.required': 'Promotion Set Code is required',
+						})
 					}).optional()
 				}),
 			)
